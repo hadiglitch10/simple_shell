3262bc8b2e8d8b5841030964469e1ab7acd7f6d3
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-07-17 13:41:53 +0300

    my shell prototype

--- my_shell.h
+++ my_shell.h
@@ -41,7 +41,120 @@
 /* External variables */
 extern char **environ;
 
-/* Function prototypes (if applicable) */
-/* void function_name(void); */
+
+
+
+
+/*structs*/
+
+/* one for linked lists */
+/**
+ * struct lnkdstr - singly linked list
+ * @numf: num field
+ * @str: string
+ * @nxt: pointer next node
+ */
+
+typedef struct lnkdstr
+{
+	struct lnkdstr *nxt; /*pointer to the nexxt node*/
+	int numf; /*num of feilds*/
+	char *str; /*string holder*/
+
+} list_s;
+
+/*one for the arguments*/
+
+/**
+ * struct passinfo - Contains pseudo-arguments to pass into a function,
+ * @cmd_type:     (CMD_NORMAL, CMD_OR, CMD_AND, CMD_CHAIN).
+ * @infd:         File descriptor from which to read line input.
+ * @env_mod:      Flag indicating if the environment has been modified.
+ * @status:       Status of the last executed command.
+ * @err_num:      Error code for exit() calls.
+ * @histcnt:      History line number count.
+ * @line_cnt_flag:Flag to indicate whether to count the current line of input.
+ * @line_cnt:     Count of input lines.
+ * @argc:         Argument count.
+ * @arg:          Input string generated from getline containing arguments.
+ * @argv:         Array of strings generated from arg.
+ * @path:         String path for the current command.
+ * @env:          Custom modified copy of the environment variable array.
+ * @cmd_buf:      Pointer to cmd ; chain buffer, for memory management.
+ * @fname:        Program filename.
+ * @env:          local copy of the environment variables.
+ * @history:      Linked list representing the history of commands entered.
+ * @alias:        Linked list representing aliases for commands.
+ */
+
+typedef struct passinfo
+{
+	int cmd_type;    /* CMD_type ||, &&, ; */
+	int infd;          /* File descriptor from which to read line input */
+	int env_mod;     /* env modification flag */
+	int status; /*status of the last executed command */
+	int err_num;         /* Error code for exit() calls */
+	int histcnt;       /* History line number count */
+	int line_cnt_flag;  /* Flag to count the current line of input */
+	unsigned int line_cnt; /* Count of input lines */
+
+	int argc;            /* Argument count */
+	char *arg;           /* Input string from getline contain arguments */
+	char **argv;         /* Array of strings generated from arg */
+
+	char *path;          /* String path for the current command */
+	char **env;      /* Custom modified copy of the environment variable array */
+	char **cmd_buf;      /* Pointer to cmd ; chain buffer, for */
+	char *fname;       /* Program filename memory management */
+
+
+	list_s *env;         /* Linked list representing copy env var */
+	list_s *history;     /*  representing the history of commands */
+	list_s *alias;       /*  representing aliases for commands */
+} info;
+
+/* Struct definition for builtin commands */
+
+/**
+ * struct builtin - built in commands
+ * @b_cmd: str cmd
+ * @fp: func point cmd
+*/
+typedef struct builtin
+{
+	int (*fp)(info *);   /* Function pointer builtin command */
+	char *b_cmd;                  /* String representing the builtin command */
+
+
+} builtin_cmd;
+
+/*initailization of pass info*/
+
+#define INFO_INIT \
+{ \
+	0,                 /* cmd_type: CMD_type ||, &&, ; */ \
+	0,                 /* infd: File descriptor read line input */ \
+	0,                 /* env_mod: env modification flag */ \
+	0,                 /* status: Status of the last executed command */ \
+	0,                 /* err_num: Error code for exit() calls */ \
+	0,                 /* histcnt: History line number count */ \
+	0,                 /* line_cnt_flag: Flag count line of input */ \
+	0,                 /* line_cnt: Count of input lines */ \
+	0,                 /* argc: Argument count */ \
+	NULL,              /* arg: Input string  containing args */ \
+	NULL,              /* argv: Array of strings generated from arg */ \
+	NULL,              /* path: String path for the current command */ \
+	NULL,              /* env:copy of the environment variables */ \
+	NULL,              /* cmd_buf: Pointer cmd chain buffer mem manage */ \
+	NULL,              /* fname: Program filename */ \
+	NULL,              /* env: local copy of env variables */ \
+	NULL,              /* history:  history of commands entered */ \
+	NULL               /* alias:  aliases for commands */ \
+}
+
+info myinfo = INFO_INIT;
+
+
+/*functions*/
 
 #endif /* _MY_SHELL_H_ */
