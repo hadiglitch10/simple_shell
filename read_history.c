4d56c235fef4d709174bc9e8583a95484f2f460b
Hadi yasser <126474345+hadiglitch10@users.noreply.github.com>
2023-07-27 02:15:52 +0300

    Update read_history.c

--- read_history.c
+++ read_history.c
@@ -1,307 +1,163 @@
-#ifndef _MY_SHELL_H_
-#define _MY_SHELL_H_
-
-/* Constants and macros */
-
-/*alias*/
-#define MAX_ALIAS_LEN 100
-
-/* exits */
-#define EXIT_PERMISSION_DENIED 126
-
-/* Buffer sizes for reading and writing */
-#define BUFFER_SIZE 1024
-#define BUFFER_FLUSH -1
-
-/* Command chaining */
-#define CMD_NORMAL 0
-#define CMD_OR_OPERATOR 1
-#define CMD_AND_OPERATOR 2
-#define CMD_CHAIN_OPERATOR 3
-
-/* Convert number . */
-#define CONVERT_TO_LOWERCASE 1
-#define CONVERT_TO_UNSIGNED 2
-
-/* Flags  custom getline and strtok functions  */
-#define USE_CUSTOM_GETLINE 0
-#define USE_CUSTOM_STRTOK 0
-
-/* File name and maximum size for command history storage. */
-#define HISTORY_FILE ".my_shell_history"
-#define MAX_HISTORY_SIZE 4096
-
-/* Standard library includes */
-
-#include <stdio.h>      /* Input/Output functions printf and scanf. */
-#include <stdlib.h>     /* Memory allocation  */
-#include <unistd.h>     /* System call functions like fork, exec, and chdir. */
-#include <string.h>     /* functions like strcmp and strtok. */
-#include <sys/types.h>  /* Data types used in system calls. */
-#include <sys/wait.h>   /* Waiting for child processes. */
-#include <sys/stat.h>   /* File status and mode definitions. */
-#include <sysexits.h>
-#include <fcntl.h>      /* File control options (e.g., open flags). */
-#include <limits.h>     /* Limits of various data types. */
-#include <errno.h>      /* Error handling and reporting. */
-#include <stdbool.h>
-#include <signal.h>
-#include <ctype.h>
-
-/* External variables */
-extern char **environ;
-
-
-
-/*structs*/
-
-/* one for linked lists */
-/**
- * struct lnkdstr - singly linked list
- * @numf: num field
- * @str: string
- * @nxt: pointer next node
- */
-
-typedef struct lnkdstr
-{
-	struct lnkdstr *nxt; /*pointer to the nexxt node*/
-	int numf; /*num of feilds*/
-	char *str; /*string holder*/
-
-} list_s;
-
-/*one for the arguments*/
+#include "my_shell.h"
 
 /**
- * struct passinfo - Contains pseudo-arguments to pass into a function,
- * @cmd_type:     (CMD_NORMAL, CMD_OR, CMD_AND, CMD_CHAIN).
- * @rdfd:         File descriptor from which to read line input.
- * @env_modify:      Flag indicating if the environment has been modified.
- * @status:       Status of the last executed command.
- * @err_num:      Error code for exit() calls.
- * @histcnt:      History line number count.
- * @line_cnt_flag:Flag to indicate whether to count the current line of input.
- * @line_cnt:     Count of input lines.
- * @argc:         Argument count.
- * @arg:          Input string generated from getline containing arguments.
- * @argv:         Array of strings generated from arg.
- * @path:         String path for the current command.
- * @modified_env: Custom modified copy of the environment variable array.
- * @cmd_buffer:      Pointer to cmd ; chain buffer, for memory management.
- * @fname:        Program filename.
- * @env_list:          local copy of the environment variables.
- * @history:      Linked list representing the history of commands entered.
- * @alias:        Linked list representing aliases for commands.
+ * open_history_file - open the history file and get its size.
+ * @filename: The filename of the history file.
+ * @file_size: Pointer to store the size of the history file.
+ *
+ * Return: The file descriptor on success, or -1 on failure.
  */
-
-typedef struct passinfo
+int open_history_file(const char *filename, ssize_t *file_size)
 {
-	int cmd_type;
-	int rdfd;
-	int env_modify;
-	int status;
-	int err_num;
-	int histcnt;
-	int line_cnt_flag;
-	unsigned int line_cnt;
-	int argc;
-	char *arg;
-	char **argv;
-	char *path;
-	char **modified_env;
-	char **cmd_buffer;
-	char *fname;
-	list_s *env_list;
-	list_s *history;
-	list_s *alias;
-} info;
+	struct stat st;
+	int fd = open(filename, O_RDONLY);
 
+	if (fd == -1 || fstat(fd, &st) == -1)
+	{
+		perror("open_history_file: Failed to open or get file size");
+		return (-1);
+	}
 
-/* Struct definition for builtin commands */
-
+	*file_size = st.st_size;
+	return (fd);
+}
 /**
- * struct builtin - built-in command and its function pointer.
- * @cmd: The command string.
- * @func: Pointer to the function implementing the command.
+ * read_history_file - read the history file into a buffer.
+ * @fd: The file descriptor of the history file.
+ * @file_size: The size of the history file.
+ * @bytes_read: Pointer to store the number of bytes read from the file.
+ *
+ * Return: contents of the history file, or NULL on failure.
  */
-typedef struct builtin
+char *read_history_file(int fd, ssize_t file_size, ssize_t *bytes_read)
 {
-	char *cmd;
-	int (*func)(info *);
-} builtin_cmd;
+	char *buf = malloc(file_size + 1);
 
-/*initailization of pass info*/
+	if (!buf)
+	{
+		perror("read_history_file: malloc failed");
+		return (NULL);
+	}
 
-#define INFO_INIT \
-{ \
-	0,                 /* cmd_type: CMD_type ||, &&, ; */ \
-	0,                 /* rdfd: File descriptor read line input */ \
-	0,                 /* env_modify: env modification flag */ \
-	0,                 /* status: Status of the last executed command */ \
-	0,                 /* err_num: Error code for exit() calls */ \
-	0,                 /* histcnt: History line number count */ \
-	0,                 /* line_cnt_flag: Flag count line of input */ \
-	0,                 /* line_cnt: Count of input lines */ \
-	0,                 /* argc: Argument count */ \
-	NULL,              /* arg: Input string containing args */ \
-	NULL,              /* argv: Array of strings generated from arg */ \
-	NULL,              /* path: String path for the current command */ \
-	NULL,              /* modified_env: Copy of the environment variables */ \
-	NULL,              /* cmd_buffer:cmd ; chain buffer, for memory manage */ \
-	NULL,              /* fname: Program filename memory management */ \
-	NULL,              /* env_list: Linked list representing copy of env var */ \
-	NULL,              /* history: representing the history of commands */ \
-	NULL               /* alias: representing aliases for commands */ \
+	*bytes_read = read(fd, buf, file_size);
+	buf[*bytes_read] = '\0';
+
+	if (*bytes_read <= 0)
+	{
+		perror("read_history_file: read failed");
+		free(buf);
+		return (NULL);
+	}
+
+	return (buf);
+}
+/**
+ * process_history_buffer - process history buffer and create entries.
+ * @myinfo: Pointer to the info struct contain shell information.
+ * @buf: The history buffer contents of the history file.
+ * @bytes_read: The number of bytes read from the history file.
+ *
+ * Return: The number of history entries processed.
+ */
+int process_history_buffer(info *myinfo, const char *buf, ssize_t bytes_read)
+{
+	int i, last = 0, linecount = 0;
+
+	for (i = 0; i < bytes_read; i++)
+	{
+		if (buf[i] == '\n')
+		{
+			char *cmd = extract_command_from_buffer(buf, last, i);
+
+			if (cmd)
+			{
+				start_history_list(myinfo, cmd, linecount);
+				free(cmd);
+				linecount++;
+			}
+			else
+			{
+				perror("process_history_buffer: malloc failed");
+				return (linecount);
+			}
+			last = i + 1;
+		}
+	}
+
+	if (last != i)
+	{
+		char *cmd = extract_command_from_buffer(buf, last, i);
+
+		if (cmd)
+		{
+			start_history_list(myinfo, cmd, linecount++);
+			free(cmd);
+			linecount++;
+		}
+		else
+		{
+			perror("process_history_buffer: malloc failed");
+			return (linecount);
+		}
+	}
+	return (linecount);
 }
 
+/**
+ * extract_command_from_buffer -extract a command from the buffer.
+ * @buf: The history buffer contents of the history file.
+ * @start: The start index of the command in the buffer.
+ * @end: The end index of the command in the buffer.
+ *
+ * Return: A newly string the command, or NULL on failure.
+ */
+char *extract_command_from_buffer(const char *buf, int start, int end)
+{
+	int cmd_len = end - start + 1;
+	char *cmd = malloc(cmd_len + 1);
 
+	if (cmd)
+	{
+		str_copy_n(cmd, buf + start, cmd_len);
+		cmd[cmd_len] = '\0';
+	}
 
+	return (cmd);
+}
 
-/*functions*/
+/**
+ * read_history - Reads history file.
+ * @myinfo: Pointer to the info struct containing shell information.
+ *
+ * Return: The number of history entries read on success, 0 otherwise.
+ */
+int read_history(info *myinfo)
+{
+	char *buf = NULL;
+	ssize_t bytes_read, file_size = 0;
+	int linecount = 0, file_fd;
+	char *filename = get_history_file(myinfo);
 
-/* my info management*/
-void null_info(info *myinfo);
-char **create_argv_manually(info *myinfo, char *arg);
-void initialize_info(info *myinfo);
-void myinfo_free(info *myinfo, int freeall);
+	if (!filename)
+		return (0);
+	file_fd = open_history_file(filename, &file_size);
 
-/*env management*/
-char **copy_env(info *myinfo);
-int env_new_app(info *myinfo, char *variable, char *value);
-int remove_env(info *myinfo, char *var_2be_removed);
-int current_env(info *myinfo);
-char *search_env_value(info *myinfo, const char *wanted_var);
-int new_env(info *myinfo);
-int share_env_list(info *myinfo);
-int my_unsetenv(info *myinfo);
-
-/*cmd_management*/
-int is_cmd_exec(info *myinfo,const char *path);
-char *duplicate_substring(const char *str, int start, int end);
-char *find_cmd_path(info *myinfo, const char *pathstr, const char *command);
-void handle_cmd_found(info *myinfo, char *path);
-void handle_cmd_not_found(info *myinfo);
-
-/*interactive shell*/
-int find_builtin_command(info *myinfo);
-void execute_cmd(info *myinfo);
-void find_and_execute_cmd(info *myinfo);
-int run_shell(info *myinfo);
-
-/*builtin_functions*/
-int builtin_help(info *myinfo);
-int builtin_exit(info *myinfo);
-int _cd(info *myinfo);
-char *duplicate_substring(const char *str, int start, int end);
-
-
-/*alias mangment*/
-bool unset_alias(info *myinfo, char *alias);
-int set_alias(info *myinfo, char *str);
-int _myalias(info *myinfo);
-int show_history(info *myinfo);
-bool show_alias(const list_s *alias_node);
-
-/*history management*/
-int write_history(info *myinfo);
-char *get_history_file(info *myinfo);
-void start_history_list(info *myinfo, char *buf, int line_count);
-int renum_history(info *myinfo);
-
-/*read history*/
-int open_history_file(const char *filename, ssize_t *file_size);
-char *read_history_file(int fd, ssize_t file_size, ssize_t *bytes_read);
-int process_history_buffer(info *myinfo, const char *buf, ssize_t bytes_read);
-char *extract_command_from_buffer(const char *buf, int start, int end);
-int read_history(info *myinfo);
-
-/*getline*/
-void control_c(int sig_num);
-ssize_t read_buffer(info *myinfo, char *buf, size_t *bytes_read);
-ssize_t input_buf(info *myinfo, char **buf, size_t *len);
-ssize_t get_input(info *myinfo);
-int my_get_next_line(info *myinfo, char **buffer, size_t *size);
-
-/*tokenizer*/
-int count_words(const char *str, char delimiter);
-char **allocate_memo(int num_words);
-int extract_word(const char *str, int i, char delimiter);
-char **split_str_to_words(const char *str, char delimiter);
-
-
-/*stringf1*/
-int string_cmp(char *str1, char *str2);
-char *str_copy_n(char *destin, const char *sorc, int n);
-char *string_dupli(const char *str);
-int string_length(const char *str);
-char *string_cpy(char *destin, const char *sorc);
-
-char *string_concat(char *dest, const char *src);
-char *begin_with(const char *haystack, const char *needle);
-int put_char(char c);
-void print_str(char *input_string);
-char *str_concat_n(char *destin, const char *sorc, int n);
-
-/*reallocate*/
-void *reallocate(void *ptr, unsigned int old_size, unsigned int new_size);
-char *memofill(char *s, char b, unsigned int n);
-void funfree(char **pp);
-int freememo(void **ptr);
-
-/*linked list*/
-size_t print_link_list(const list_s *h);
-size_t print_str_list(const list_s *head);
-size_t list_length(const list_s *h);
-list_s *Add_node_begin(list_s **head, const char *str, int num);
-list_s *Add_node_end(list_s **head, const char *str, int num);
-
-ssize_t get_node_index(list_s *head, list_s *node);
-char **list_string(list_s *head);
-int delete_node(list_s **head, unsigned int index);
-list_s *node_starts_with(list_s *head, char *prefix, char c);
-void free_link_list(list_s **head_ptr);
-
-/*handle errors*/
-int put_char_err(char ch);
-int put_char_fd(char ch, int file_d);
-int put_str_fd(char *str, int file_d);
-void put_str_err(const char *str);
-
-char *convert_num(long int num, int base, int flag);
-int str_to_int(char *str);
-int print_decimal(int num, int file_d);
-void remove_comments(char *input_buff);
-void print_error(info *info, char *str_err);
-
-/*delimeter*/
-void check_chain(info *info, char *buff, char *b, size_t a, size_t len);
-int chain(info *info, char *buff, size_t *p);
-int replace_variables(info *info);
-int rep_alias(info *info);
-int rep_str(char **old, char *new);
-
-/*convert int*/
-int valid_digit_or_whitespace(char c);
-int get_sign( char **str);
-unsigned long parse_number(const char *str);
-int handle_conversion(char *str);
-int convert_to_int(char *str);
-
-/*checks*/
-int check_alpha(int character);
-int check_delim(char character, char *delim);
-int interactive_shell(info *info);
-char *str_char(char *str, char target);
-
-static const builtin_cmd builtintbl[] = {
-	{"exit", builtin_exit},
-	{"env", current_env},
-	{"help", builtin_help},
-	{"history", show_history},
-	{"setenv", new_env},
-	{"unsetenv", my_unsetenv},
-	{"cd", _cd},
-	{NULL, NULL}
-};
-#endif /* _MY_SHELL_H_ */
+	free(filename);
+	if (file_fd == -1)
+		return (0);
+	buf = read_history_file(file_fd, file_size, &bytes_read);
+	close(file_fd);
+	if (buf == NULL || bytes_read <= 0)
+	{
+		free(buf);
+		return (0);
+	}
+	linecount = process_history_buffer(myinfo, buf, bytes_read);
+	free(buf);
+	while (myinfo->histcnt > MAX_HISTORY_SIZE)
+	{
+		delete_node(&(myinfo->history), 0);
+		myinfo->histcnt--;
+	}
+	return (linecount);
+}
